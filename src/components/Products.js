aef2456f372f6fd084e651376a86f3205506613d
LaureanoVera <laureanoigvera@gmail.com>
2021-08-01 21:35:03 -0300

    development

---
+++ src/components/Products.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import
+
+const Products = () => {
+  return (
+    <ProductsContainer>
+      <ProductsHeading>Heading</ProductsHeading>
+      <ProductWrapper>
+
+      </ProductWrapper>
+    </ProductsContainer>
+  )
+}
+
+export default Products
