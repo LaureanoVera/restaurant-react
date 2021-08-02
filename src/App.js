e312c61f67320545444cceca6fe78804046c7db8
LaureanoVera <laureanoigvera@gmail.com>
2021-08-02 12:07:00 -0300

    products components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router } from 'react-router-dom'
+import { productData } from './components/Data';
 import { GlobalStyle } from "./components/GlobalStyles";
 import Hero from "./components/Hero";
+import Products from './components/Products';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="Choose your favorite" data={productData} />
     </Router>
   );
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import
+import React from "react";
+import {
+  ProductsContainer,
+  ProductsHeading,
+  ProductWrapper,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+  ProductTitle,
+  ProductDesc,
+  ProductPrice,
+  ProductButton,
+} from "./ProductsElements";
 
-const Products = () => {
+const Products = ({ heading, data }) => {
   return (
     <ProductsContainer>
-      <ProductsHeading>Heading</ProductsHeading>
+      <ProductsHeading>{heading}</ProductsHeading>
       <ProductWrapper>
-
+        {data.map((el, index) => {
+          return (
+            <ProductCard key={index}>
+              <ProductImg src={el.img} alt={el.alt} />
+              <ProductInfo>
+                <ProductTitle>{el.name}</ProductTitle>
+                <ProductDesc>{el.desc}</ProductDesc>
+                <ProductPrice>{el.price}</ProductPrice>
+                <ProductButton>{el.button}</ProductButton>
+              </ProductInfo>
+            </ProductCard>
+          );
+        })}
       </ProductWrapper>
     </ProductsContainer>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
